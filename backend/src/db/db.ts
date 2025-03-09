a5238e5d26022b05384d89a7cdfd4548d8a882bb
Madiba Hudson-Quansah <mhquansah@gmail.com>
2025-03-09 01:51:17 +0000

    Build fix fr

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = postgres(process.env.DATABASE_URL || "postgres://USER:PASS@localhost:5432/poketeam ", {
+    ssl: false,
     prepare: true,
     transform: {
         undefined: null,
